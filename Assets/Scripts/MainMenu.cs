b856c89422360894eea2771399fd7be33092a698
bobby-the-epic <174213631+bobby-the-epic@users.noreply.github.com>
2024-07-19 21:03:03 -0500

    Added main menu title screen and renamed scene

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public Slider volumeSlider;
+    public GameObject optionsScreen, titleScreen;
+
+    public void Start()
+    {
+        volumeSlider.value = GameManager.volume;
+    }
+    public void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape) && optionsScreen.activeInHierarchy)
+        {
+            optionsScreen.SetActive(false);
+            titleScreen.SetActive(true);
+        }
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("FishingGame");
+    }
+    public void VolumeAdjust()
+    {
+        GameManager.volume = volumeSlider.value;
+    }
+    public void OptionsMenu()
+    {
+        titleScreen.SetActive(false);
+        optionsScreen.SetActive(true);
+    }
+}
